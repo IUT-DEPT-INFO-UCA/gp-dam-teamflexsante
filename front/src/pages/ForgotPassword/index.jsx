373bfb88d15bcf217ad4b825434474e4e1db9ddc
Dragun06 <dylan.lafaire.ts2@gmail.com>
2022-10-06 11:59:39 +0200

    :sparkles: #21 Integration forget password

--- front/src/pages/ForgotPassword/index.jsx
+++ front/src/pages/ForgotPassword/index.jsx
@@ -1,7 +1,14 @@
-import { Box, Card } from '@mui/material'
+import { Box, Button, Card, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 const ForgotPassword = () => {
+  const { t } = useTranslation()
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    console.log(data)
+  }
   return (
     <Box
       sx={{
@@ -18,7 +25,25 @@ const ForgotPassword = () => {
           padding: 3,
           textAlign: 'center'
         }}>
-        TODO
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Typography component="h1" variant="h5">
+            {t('forgotPassword.title')}
+          </Typography>
+          <Typography align="left" component="p" margin="normal" sx={{ mt: 3, mb: 1 }} variant="p">
+            {t('forgotPassword.text')}
+          </Typography>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label={t('forgotPassword.email')}
+            name="email"
+          />
+          <Button type="submit" variant="contained" color="secondary" sx={{ mt: 3, mb: 2 }}>
+            {t('forgotPassword.submit')}
+          </Button>
+        </Box>
       </Card>
     </Box>
   )
