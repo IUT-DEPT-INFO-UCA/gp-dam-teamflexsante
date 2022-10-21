a3d087f326b7b653cab75a701608e7d0ddf662cd
Dragun06 <dylan.lafaire.ts2@gmail.com>
2022-10-21 16:07:18 +0200

    Start integration health info

--- front/src/components/HealthInfo/index.jsx
+++ front/src/components/HealthInfo/index.jsx
@@ -1,11 +1,108 @@
+import { Box, Card, Typography } from '@mui/material'
 import React from 'react'
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
+import { useTranslation } from 'react-i18next'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
-const HealthInfo = () => {
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+]
+
+const HealthInfo = (props) => {
+  const { t } = useTranslation()
+  const { user } = props
+  console.log(user)
   return (
-    <div>
-      <h1>HealthInfo</h1>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '80vh',
+        backgroundColor: 'background.default'
+      }}>
+      <Card
+        sx={{
+          width: '100%',
+          maxWidth: 500,
+          padding: 3
+        }}>
+        <Typography component="h1" variant="h5">
+          {t('healthInfo.title')}
+        </Typography>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5
+          }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </Card>
+    </Box>
   )
 }
 
-export default HealthInfo
+HealthInfo.propTypes = {
+  user: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user.user
+})
+
+const mapDispatchToProps = () => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HealthInfo)
