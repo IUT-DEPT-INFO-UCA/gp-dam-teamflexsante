2f3df8845063a4cae6bf3e48dda75099c2e93923
Tryns <113093354+Tryns@users.noreply.github.com>
2023-01-24 14:13:04 +0100

    ✨ #68 Add integration for Login page

--- front/src/Layout/index.jsx
+++ front/src/Layout/index.jsx
@@ -1,47 +1,48 @@
 import React, { useEffect } from 'react'
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
+import { Route, Routes, unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import { RELOAD_USER } from '../redux/store/user/actions'
-import { CLEAR_ERROR, CLEAR_VALIDATION } from '../redux/store/app/slice'
+// import { RELOAD_USER } from '../redux/store/user/actions'
+// import { CLEAR_ERROR, CLEAR_VALIDATION } from '../redux/store/app/slice'
 import history from '../router/history'
 
 import Error from '../components/Error'
 import Validation from '../components/Validation'
+import { routes } from '../router/routes'
+import Login from '../pages/Login'
 
-const Layout = (props) => {
-  const { validation, error, resetError, resetValidation, reloadUser } = props
+const Layout = () => {
 
-  useEffect(() => {
-    resetError()
-    resetValidation()
-    reloadUser()
-  }, [resetError, resetValidation])
+  // useEffect(() => {
+  //   resetError()
+  //   resetValidation()
+  //   reloadUser()
+  // }, [resetError, resetValidation])
 
-  if (error || validation) {
-    setTimeout(() => {
-      resetError()
-      resetValidation()
-    }, 5000)
-  }
+  // if (error || validation) {
+  //   setTimeout(() => {
+  //     resetError()
+  //     resetValidation()
+  //   }, 5000)
+  // }
 
   return (
     <HistoryRouter history={history}>
-      <Validation validation={validation} resetValidation={resetValidation} />
-      <Error error={error} resetError={resetError} />
-      {/* ROUTES */}
+      <Routes>
+        <Route path={routes.login} element={<Login />} />
+      </Routes>
     </HistoryRouter>
   )
 }
 
 Layout.propTypes = {
-  validation: PropTypes.string.isRequired,
-  error: PropTypes.string.isRequired,
-  user: PropTypes.object.isRequired,
-  resetError: PropTypes.func.isRequired,
-  resetValidation: PropTypes.func.isRequired,
+  validation: PropTypes.string,
+  error: PropTypes.string,
+  user: PropTypes.object,
+  resetError: PropTypes.func,
+  resetValidation: PropTypes.func,
 }
 
 const mapStateToProps = (state) => ({
@@ -51,8 +52,8 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  resetError: () => dispatch({ type: CLEAR_ERROR }),
-  resetValidation: () => dispatch({ type: CLEAR_VALIDATION }),
+  // resetError: () => dispatch({ }),
+  // resetValidation: () => dispatch({ }),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Layout)
